c56479919cbb78af2ffc86dc4c0487b9b3c8e2df
bas081 <bas080@hotmail.com>
2024-06-14 16:48:39 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! Allow long press for share button

---
+++ docs/serviceWorker.e5379146.js
@@ -0,0 +1,2 @@
+const e="toplist-v1",t=["/","/index.html","/toplist/","/toplist/index.html"];self.addEventListener("install",a=>{a.waitUntil(caches.open(e).then(e=>{e.addAll(t)}))}),self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(t=>t||fetch(e.request)))}),self.addEventListener("message",t=>{t.data&&"clearCache"===t.data.action&&caches.delete(e).then(()=>{console.log(`${e} cache cleared`)})});
+//# sourceMappingURL=serviceWorker.e5379146.js.map
