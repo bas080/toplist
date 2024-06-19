35c71978a0b61c5a8cf564a45635188ec30c54fe
bas080 <bas080@users.noreply.github.com>
2024-06-19 14:50:08 +0000

    deploy: 209ea6df860519cd649b6ad6f7358c4b0813cb9d

--- serviceWorker.js
+++
@@ -1,2 +0,0 @@
-const e={undefined(e){console.error(`No action with name "${e.data.action}".`)},clearCache(){caches.keys().then(function(e){for(let t of e)caches.delete(t)})}};self.addEventListener("fetch",async({request:e})=>{try{let t=await caches.match(e);if(t)return t;return await fetch(e)}catch(e){throw console.error("Fetch error:",e),e}}),self.addEventListener("message",t=>{e[t.data.action](t)});
-//# sourceMappingURL=serviceWorker.js.map
